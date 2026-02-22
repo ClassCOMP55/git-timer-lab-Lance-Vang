d1b239935b3c43fb4268ad54b7d814d655c29cd3
Lance Vang <l_vang21@u.pacific.edu>
2026-02-22 13:54:23 -0800

    "Enemies populated on screen regularly"

--- src/BallLauncher.java
+++ src/BallLauncher.java
@@ -5,10 +5,15 @@ import java.awt.event.*;
 import java.util.ArrayList;
 import javax.swing.*;
 
-public class BallLauncher extends GraphicsProgram{
+public class BallLauncher extends GraphicsProgram implements ActionListener{
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int SIZE = 25;
+	
+	private ArrayList<GOval>balls;
+	private Timer movement; 
+	private static final int MS = 20;
+	private static final int SPEED = 5; 
 	
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
@@ -16,10 +21,19 @@ public class BallLauncher extends GraphicsProgram{
 	}
 	
 	public void run() {
+		balls = new ArrayList<GOval>();
 		addMouseListeners();
+		
+		movement = new Timer(MS, this);
+		movement.start();
 	}
 	
 	public void mousePressed(MouseEvent e) {
+		for(GOval ball : balls) {
+			if (ball.getX() < 100) {
+				return; 
+			}
+		}
 		GOval ball = makeBall(SIZE/2, e.getY());
 		add(ball);
 	}
@@ -28,7 +42,15 @@ public class BallLauncher extends GraphicsProgram{
 		GOval temp = new GOval(x-SIZE/2, y-SIZE/2, SIZE, SIZE);
 		temp.setColor(Color.RED);
 		temp.setFilled(true);
-		return temp;
+		
+		balls.add(temp);
+		return temp; 
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		for(GOval ball : balls) {
+			ball.move(SPEED, 0);
+		}
 	}
 	
 	public static void main(String[] args) {
